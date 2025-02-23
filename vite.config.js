0b23199e469b640bc5ea0eea4791b33cd8a40cc2
Frontend-Umidjon <umidjonkengashev6@gmail.com>
2025-02-23 23:08:59 +0500

    PWA

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
