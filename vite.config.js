33a65a10d55f471e54e0539de0a9cdaa91164e99
Frontend-Umidjon <umidjonkengashev6@gmail.com>
2025-03-07 12:40:33 +0500

    PWA fixed

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,29 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({
-    srcDir: 'src',
-    filename: 'sw.js',
-    devOptions: {
-      enabled: true,
-      type: 'module',
-    },
-    strategies: 'injectManifest',
-    injectManifest: {
-      injectionPoint: undefined
+  plugins: [react(), tailwindcss(),  VitePWA({
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+      name: 'My Awesome App',
+      short_name: 'MyApp',
+      description: 'My Awesome App description',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
     }
   }),],
   resolve: {
